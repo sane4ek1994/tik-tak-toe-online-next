d301a8deef7965bde58df31adb8026ea921263c9
Alexandr Naumov <mfq07k9cz@gmail.com>
2024-12-23 16:51:30 +0300

    added Prisma and tsx prisma pack

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    await prisma.game.create({
+        data: {
+            name: "Game-1",
+
+        }
+    })
+    await prisma.game.create({
+        data: {
+            name: "Game-2",
+
+        }
+    })
+    await prisma.game.create({
+        data: {
+            name: "Game-3",
+
+        }
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { prisma } from "@/shared/lib/db";
 import {Button} from "@/shared/ui/button";
 
-export default function Home() {
+export default async function Home() {
+    const games = await prisma.game.findMany()
+    console.log(games)
   return (
     <div>
      <Button>Click me</Button>
