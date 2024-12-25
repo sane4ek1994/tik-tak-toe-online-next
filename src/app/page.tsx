a71c0b5b0fdd6dfb51c4bcb9da7fd11d55861c0b
Alexandr Naumov <mfq07k9cz@gmail.com>
2024-12-25 21:58:39 +0300

    created dotenv-sli and settings prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { prisma } from "@/shared/lib/db";
-import {Button} from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
 export default async function Home() {
-    const games = await prisma.game.findMany()
-    console.log(games)
+  const games = await prisma.game.findMany();
+  console.log(games);
   return (
     <div>
-     <Button>Click me</Button>
+      <Button>Hello world</Button>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
