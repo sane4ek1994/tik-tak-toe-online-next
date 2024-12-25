def815d6d973655ddc366481b0882a3a91fefea2
Alexandr Naumov <mfq07k9cz@gmail.com>
2024-12-25 21:58:54 +0300

    created dotenv-sli and settings prettier

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,29 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 async function main() {
-    await prisma.game.create({
-        data: {
-            name: "Game-1",
-
-        }
-    })
-    await prisma.game.create({
-        data: {
-            name: "Game-2",
-
-        }
-    })
-    await prisma.game.create({
-        data: {
-            name: "Game-3",
-
-        }
-    })
+  await prisma.game.create({
+    data: {
+      name: "Game-1",
+    },
+  });
+  await prisma.game.create({
+    data: {
+      name: "Game-2",
+    },
+  });
+  await prisma.game.create({
+    data: {
+      name: "Game-3",
+    },
+  });
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
