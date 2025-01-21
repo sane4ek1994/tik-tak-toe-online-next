561f45c779bce2f79f25c07cc20974b222d48502
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-21 10:09:58 +0300

    feat: added user repositories

---
+++ src/entities/user/repositories/user.ts
@@ -0,0 +1,19 @@
+import { UserEntity } from "@/entities/user/services/domain";
+import { prisma } from "@/shared/lib/db";
+import { Prisma } from "@prisma/client";
+
+export function saveUser(user: UserEntity): Promise<UserEntity> {
+  return prisma.user.upsert({
+    where: {
+      id: user.id,
+    },
+    create: user,
+    update: user,
+  });
+}
+
+export function getUser(where: Prisma.UserWhereInput) {
+  return prisma.user.findFirst({ where });
+}
+
+export const userRepository = { saveUser, getUser };
