7b48bfdd677ba0d2c229fa235299d6d01ae6343a
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-21 11:10:18 +0300

    feat: added JSdoc

--- src/entities/user/services/password.ts
+++ src/entities/user/services/password.ts
@@ -1,16 +1,52 @@
+/**
+ * Утилиты для хеширования и проверки паролей.
+ * Код вдохновлен примером с GeeksforGeeks:
+ * https://geeksforgeeks.org/node-js-password-hashing-crypto-module/
+ *
+ * Данный модуль предоставляет функции для безопасного хеширования паролей
+ * и проверки введенных паролей по хешу.
+ *
+ * @module passwordService
+ */
+
 import { pbkdf2, randomBytes } from "node:crypto";
 
-function hashPassword(
+/**
+ * Хеширует пароль с использованием PBKDF2 и соли.
+ *
+ * @async
+ * @function hashPassword
+ * @param {string} password - Пароль в виде обычного текста, который нужно захешировать.
+ * @param {string} [salt=randomBytes(12).toString("hex")] - Необязательный параметр соли. Если соль не указана, генерируется новая.
+ * @returns {Promise<{hash: string, salt: string}>} - Объект, содержащий хеш пароля и использованную соль.
+ */
+async function hashPassword(
   password: string,
-  salt = randomBytes(12).toString("hex"),
-) {
-  //Промисификация криптографии
-  return new Promise<Buffer>((resolve, reject) =>
+  salt: string = randomBytes(12).toString("hex"),
+): Promise<{ hash: string; salt: string }> {
+  const hash = await new Promise<Buffer>((resolve, reject) =>
     pbkdf2(password, salt, 1000, 64, `sha512`, (error, value) =>
       error ? reject(error) : resolve(value),
     ),
-  ).then((r) => r.toString(`hex`));
+  );
+
+  return {
+    hash: hash.toString(`hex`),
+    salt,
+  };
 }
+
+/**
+ * Проверяет соответствие пароля и хеша.
+ *
+ * @async
+ * @function comparePassword
+ * @param {Object} params - Параметры для проверки.
+ * @param {string} params.password - Пароль в виде обычного текста, который нужно проверить.
+ * @param {string} params.hash - Хеш пароля, с которым нужно сравнить.
+ * @param {string} params.salt - Соль, использованная при хешировании пароля.
+ * @returns {Promise<boolean>} - Возвращает `true`, если пароль соответствует хешу, иначе `false`.
+ */
 
 async function comparePassword({
   hash,
@@ -20,8 +56,8 @@ async function comparePassword({
   password: string;
   hash: string;
   salt: string;
-}) {
-  return hash === (await hashPassword(password, salt));
+}): Promise<boolean> {
+  return hash === (await hashPassword(password, salt)).hash;
 }
 
 export const passwordService = { comparePassword, hashPassword };
