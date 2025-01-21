d25c11fd3dd543668d2c225190ef5daa53637885
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-21 09:46:40 +0300

    feat: user and game ids

---
+++ src/entities/user/services/domain.ts
@@ -0,0 +1,14 @@
+import { UserId } from "@/kernel/ids";
+
+export type UserEntity = {
+  id: UserId;
+  login: string;
+  rating: number;
+  passwordHash: string;
+  salt: string;
+};
+
+export type SessionEntity = {
+  id: UserId;
+  login: string;
+};
