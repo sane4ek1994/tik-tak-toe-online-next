dea84e578a14e77aac27ac4821500e5da7f808c4
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-21 11:14:38 +0300

    return user

--- src/entities/user/services/create-user.ts
+++ src/entities/user/services/create-user.ts
@@ -1,5 +1,5 @@
 import { userRepository } from "@/entities/user/repositories/user";
-import { left } from "@/shared/lib/either";
+import { left, right } from "@/shared/lib/either";
 import cuid from "cuid";
 import { DEFAULT_RATING } from "@/entities/user/domain";
 import { passwordService } from "@/entities/user/services/password";
@@ -19,11 +19,13 @@ export const createUser = async ({
 
   const { hash, salt } = await passwordService.hashPassword(password);
 
-  userRepository.saveUser({
+  const user = await userRepository.saveUser({
     id: cuid(),
     login,
     rating: DEFAULT_RATING,
     passwordHash: hash,
     salt,
   });
+
+  return right(user);
 };
