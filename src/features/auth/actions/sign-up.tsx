fcade878236b440f8203845a70af12e321cdd96d
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-24 14:57:55 +0300

    feat: created Session from users with JWT

--- src/features/auth/actions/sign-up.tsx
+++ src/features/auth/actions/sign-up.tsx
@@ -1,6 +1,26 @@
-import { left } from "@/shared/lib/either";
+import { left, mapLeft } from "@/shared/lib/either";
+import { z } from "zod";
+import { createUser } from "@/entities/user/server";
+
+const formDataSchema = z.object({
+  login: z.string().min(3),
+  password: z.string().min(3),
+});
 
 export const signUpAction = async (state: unknown, formData: FormData) => {
   console.log(formData.get("login"), formData.get("password"));
-  return left("login-all-ready-taken" as const);
+  const data = Object.fromEntries(formData.entries());
+
+  const result = formDataSchema.safeParse(data);
+  if (!result.success) {
+    return left(`${result.error.message}`);
+  }
+
+  const createUserResult = await createUser(result.data);
+
+  return mapLeft(createUserResult, (error) => {
+    return {
+      "user-login-exists": "Пользователь с таким login существует",
+    }[error];
+  });
 };
