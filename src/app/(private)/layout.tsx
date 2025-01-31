eae6a98d10ecf5a84303854f26d2c19e0f33c712
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-01-31 15:48:43 +0300

    feat: jsdoc

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,6 +3,27 @@ import { ReactNode } from "react";
 import { sessionService } from "@/entities/user/services/session";
 import { redirect } from "next/navigation";
 
+/**
+ * Форма, предназначенная для удаления текущей пользовательской сессии и перенаправления на страницу авторизации.
+ * Использует Server Actions (Next.js 13+):
+ * - строка `"use server";` указывает, что логика внутри функции `action` выполняется на сервере;
+ * - вызов `sessionService.deleteSession()` удаляет текущую сессию;
+ * - после удаления сессии выполняется `redirect("/sign-in")`, чтобы перенаправить пользователя на страницу авторизации.
+ *
+ * @example
+ * <form
+ *   action={async () => {
+ *     "use server";
+ *     await sessionService.deleteSession();
+ *     redirect("/sign-in");
+ *   }}
+ * >
+ *   <Button>Sign out</Button>
+ * </form>
+ *
+ * @returns Возвращает HTML-форму с кнопкой, которая по нажатию удаляет сессию и перенаправляет на страницу авторизации.
+ */
+
 export default async function PrivateLayout({
   children,
 }: {
@@ -11,7 +32,7 @@ export default async function PrivateLayout({
   const { session } = await sessionService.verifySession();
 
   return (
-    <div>
+    <div className="flex flex-col grow">
       <header className="px-10 py-5 flex flex-row gap-4 justify-between border-b border-b-primary/50 items-center">
         <div className="text-xl">Tick-tack-toe-online</div>
         <div className="flex gap-4 items-center">
@@ -19,7 +40,6 @@ export default async function PrivateLayout({
           <form
             action={async () => {
               "use server";
-              console.log("logout");
               await sessionService.deleteSession();
               redirect("/sign-in");
             }}
