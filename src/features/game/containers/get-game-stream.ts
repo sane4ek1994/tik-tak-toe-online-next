ee86bc45a7f79572d4b15e684e2032da5082ba35
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-02-06 12:06:31 +0300

    feat: fix response

--- src/features/game/containers/get-game-stream.ts
+++ src/features/game/containers/get-game-stream.ts
@@ -1,5 +1,5 @@
 import { sseStream } from "@/shared/lib/sse/server";
-import { NextRequest } from "next/server ";
+import { NextRequest } from "next/server";
 
 export function getGameStream(req: NextRequest) {
   const { response, handleClose, write } = sseStream(req);
