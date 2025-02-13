dab80f09225d4e78a9138dbfe694fed0f504cdfb
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-02-13 11:08:49 +0300

    refactor: GameEventsService

--- src/features/game/containers/game-client.tsx
+++ src/features/game/containers/game-client.tsx
@@ -1,18 +1,18 @@
 "use client";
 
+import { GameDomain } from "@/entities/game";
 import { GameLayout } from "@/features/game/server";
 import { GamePlayers } from "@/features/game/ui/players";
 import { GameStatus } from "@/features/game/ui/status";
 import { GameField } from "@/features/game/ui/field";
 import { useGame } from "@/features/game/model/use-game";
-import { GameId } from "@/kernel/ids";
 
-export function GameClient({ gameId }: { gameId: GameId }) {
-  const { game, isPending } = useGame(gameId);
-
-  if (!game || isPending) {
-    return <GameLayout status={"Загрузка..."} />;
-  }
+export function GameClient({
+  defaultGame,
+}: {
+  defaultGame: GameDomain.GameEntity;
+}) {
+  const { game = defaultGame } = useGame(defaultGame.id);
 
   return (
     <GameLayout
--- src/features/game/containers/game-server.tsx
+++ src/features/game/containers/game-server.tsx
@@ -1,19 +1,28 @@
 import { GameClient } from "@/features/game/server";
 import { GameId } from "@/kernel/ids";
 import { getCurrentUser } from "@/entities/user/server";
-import { startGame } from "@/entities/game/server";
+import { getGameById, startGame } from "@/entities/game/server";
 import { gameEvents } from "@/features/game/services/game-event";
 
 export async function GameServer({ gameId }: { gameId: GameId }) {
   const user = await getCurrentUser();
 
+  let game = await getGameById(gameId);
+
+  if (!game) {
+    throw new Error("Game not found");
+  }
+
   if (user) {
     const startGameResult = await startGame(gameId, user);
 
     if (startGameResult.type === "right") {
-      gameEvents.emit();
+      game = startGameResult.value;
+      gameEvents.emit(startGameResult.value);
     }
   }
 
-  return <GameClient gameId={gameId} />;
+  console.log(game);
+
+  return <GameClient defaultGame={game} />;
 }
